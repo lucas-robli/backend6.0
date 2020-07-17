f2502e9c04df559ba660a72ac0e489c37bf3f054
fabriciopaschoal <fabricio8007@gmail.com>
2020-07-17 08:53:04 -0300

    Resolvendo erros!

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -7,9 +7,11 @@ class BoxController{
         return res.json(box);
     }
     async show(req, res){
-        const box = await Box.findById(req.params.id).populate('files');
-        path : 'files';
-        options : {sort: {createdAt: -1}}
+        const box = await Box.findById(req.params.id).populate({
+            path : 'files',
+            options : {sort: {createdAt: -1}}
+        });
+        
         return res.json(box);
     }
 }
--- src/server.js
+++ src/server.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(cors());
 
 const server = require('http').Server(app);
-const io = require('socket.io')(Server);
+const io = require('socket.io')(server);
 
-io.once('connection', socket => {
+io.on('connection', socket => {
     socket.on('connectRoom', box => {
         socket.join(box);
     })
@@ -30,8 +30,7 @@ app.use((req, res, next) => {
     return next();
 });
 
-app.use(cors());
-app.use(express());
+app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'temp')));
 
